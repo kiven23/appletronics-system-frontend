7cfe184b2402610f152fec51f0229083755ff71d
kiven23 <calimlim.steven@gmail.com>
2022-12-15 16:41:12 +0800

    updated

--- src/views/appletronics_system/booking/jobs.vue
+++ src/views/appletronics_system/booking/jobs.vue
@@ -85,7 +85,10 @@
             <v-btn icon dark @click="jobsInfo = false">
               <v-icon>mdi-close</v-icon>
             </v-btn>
-            <v-toolbar-title>Job Status Update: <strong>{{jobstat}}</strong></v-toolbar-title>
+            <v-toolbar-title
+              >Job Status Update:
+              <strong>{{ jobstatus }}</strong></v-toolbar-title
+            >
             <v-spacer></v-spacer>
             <v-toolbar-items> </v-toolbar-items>
           </v-toolbar>
@@ -94,11 +97,10 @@
             <v-list-item>
               <v-list-item-content>
                 <v-data-table
-                dense
-                :headers="unitsHeader"
-                :items="unitsData"
-              ></v-data-table>
-              
+                  dense
+                  :headers="unitsHeader"
+                  :items="unitsData"
+                ></v-data-table>
               </v-list-item-content>
             </v-list-item>
           </v-list>
@@ -110,35 +112,35 @@
                   <v-col cols="12" sm="3">
                     <v-card class="pa-2">
                       <strong>Contact Phone Number</strong><br />
-                      {{jobsData.customer.cpnumber}}<br />
+                      {{ jobsData.customer.cpnumber }}<br />
                       <strong>Last Name</strong><br />
-                      {{jobsData.customer.lastname}}<br />
+                      {{ jobsData.customer.lastname }}<br />
                       <strong>House No.</strong><br />
-                      {{jobsData.customer.houseno}}<br />
+                      {{ jobsData.customer.houseno }}<br />
                       <strong>Location of Unit</strong><br />
-                       N/A<br />
+                      N/A<br />
                     </v-card>
                   </v-col>
                   <v-col cols="12" sm="3">
                     <v-card class="pa-2">
                       <strong>Contact Email Address</strong><br />
-                      {{jobsData.customer.emailaddress}}<br />
+                      {{ jobsData.customer.emailaddress }}<br />
                       <strong>First Name</strong><br />
-                      {{jobsData.customer.firstname}}<br />
+                      {{ jobsData.customer.firstname }}<br />
                       <strong>Street</strong><br />
-                      {{jobsData.customer.street}}<br />
+                      {{ jobsData.customer.street }}<br />
                       <strong>Special Request</strong><br />
-                      {{jobsData.customer.specialinstruction}}<br />
+                      {{ jobsData.customer.specialinstruction }}<br />
                     </v-card>
                   </v-col>
                   <v-col cols="12" sm="3">
                     <v-card class="pa-2">
                       <strong>Telephone No.</strong><br />
-                      {{jobsData.customer.telephoneno}}<br />
+                      {{ jobsData.customer.telephoneno }}<br />
                       <strong>Middle Name</strong><br />
-                      {{jobsData.customer.middlename}}<br />
+                      {{ jobsData.customer.middlename }}<br />
                       <strong>Barangay</strong><br />
-                      {{jobsData.customer.barangay}}<br />
+                      {{ jobsData.customer.barangay }}<br />
                       <strong>Installer</strong><br />
                       N/A<br />
                     </v-card>
@@ -146,9 +148,9 @@
                   <v-col cols="12" sm="3">
                     <v-card class="pa-2">
                       <strong>Contact Person</strong><br />
-                      {{jobsData.customer.contactperson}}<br />
+                      {{ jobsData.customer.contactperson }}<br />
                       <strong>Municipality</strong><br />
-                      {{jobsData.customer.mcity}}<br />
+                      {{ jobsData.customer.mcity }}<br />
                       <strong>Date of Installation</strong><br />
                       N/A<br />
                       <strong>Province</strong><br />
@@ -166,7 +168,28 @@
               <v-list-item-content>
                 <v-row no-gutters>
                   <v-col cols="12" sm="3">
-                    <v-card class="pa-2" outlined tile>
+                    <strong>Sales Invoice</strong><br />
+                    <v-btn
+                      x-small
+                      color="success"
+                      @click="download(jobsData.id)"
+                    >
+                      <v-icon>mdi-attachment</v-icon> </v-btn
+                    ><br />
+                    <br />
+                    <v-card class="pa-1">
+                      <v-autocomplete
+                        v-model="tech"
+                        label="Assign Tech/Installer"
+                        :items="installer"
+                        item-text="name"
+                        item-value="value"
+                        chips
+                        dense
+                        deletable-chips
+                        small-chips
+                      ></v-autocomplete>
+                      <br />
                       <v-autocomplete
                         v-model="jobstatus"
                         label="Job Status"
@@ -174,21 +197,98 @@
                         item-text="name"
                         item-value="value"
                         chips
+                        dense
                         deletable-chips
                         small-chips
                       ></v-autocomplete>
+                      <br />
+                      <vs-input
+                        type="date"
+                        v-model="value7"
+                        label="Date of Installation / Site Visit"
+                      />
+                      <div>
+                        <br />
+                        <v-btn color="dark">
+                          <v-icon>mdi-send</v-icon>
+                        </v-btn>
+                      </div>
                     </v-card>
                   </v-col>
-                  <v-col cols="12" sm="3">
-                    <v-card class="pa-2" outlined tile>
-                      <strong>Attachment</strong><br/>
-                    </v-card>
+                  <v-col cols="12">
+                    <v-btn
+                      @click="jobUpdates()"
+                      color="dark"
+                      dark
+                      absolute
+                      top
+                      right
+                      fab
+                    >
+                      <v-icon>mdi-wechat</v-icon>
+                    </v-btn>
                   </v-col>
                 </v-row>
               </v-list-item-content>
             </v-list-item>
           </v-list>
         </v-card>
+        <v-dialog v-model="jobupdateDialog" max-width="500px">
+          <v-card>
+            <v-card-title> Job Update </v-card-title>
+
+            <v-list three-line>
+              <div v-for="(item, $index) in items" v-bind:key="$index">
+                <v-subheader>{{ item.date }}</v-subheader>
+
+                <v-divider> </v-divider>
+
+                <v-list-item>
+                  <v-list-item-avatar>
+                    <v-img :src="item.avatar"></v-img>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content>
+                    <v-list-item-title v-html="item.title"></v-list-item-title>
+
+                    <strong>{{ item.subtitle }} </strong>
+                  </v-list-item-content>
+                </v-list-item>
+              </div>
+            </v-list>
+            <v-list three-line class="pa-3">
+                      <v-autocomplete
+                        v-model="reason"
+                        label="Reason"
+                        :items="reasonItem"
+                        item-text="name"
+                        item-value="value"
+                        chips
+                        dense
+                        deletable-chips
+                        small-chips
+                      ></v-autocomplete>
+            </v-list>
+            <v-list three-line class="pa-3">
+              <v-textarea
+                outlined
+                name="input-7-4"
+                label="Enter Update Here"
+                value=""
+              ></v-textarea>
+            </v-list>
+            
+
+            <v-card-actions>
+              <v-btn color="primary" text @click="dialog2 = false">
+                CLOSE
+              </v-btn>
+              <v-btn   text @click="send()">
+                <v-icon>mdi-send</v-icon>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
       </v-dialog>
     </v-container>
   </div>
@@ -200,44 +300,61 @@ import { mapGetters } from "vuex";
 export default {
   data() {
     return {
-      unitsHeader: [
-      { text: "Product Category", value: "prodcategories" },
-      { text: "Appliance Type", value: "appliancetype" },
-      // { text: "Request Type", value: "branch.name" },
-      { text: "Brand", value: "brand" },
-      { text: "Qty", value: "qty" },
-      { text: "Model", value: "model" },
-      { text: "Serial No.", value: "serialno" },
-      { text: "Unit Condition", value: "unitcondition" },
-      { text: "Warranty Condition", value: "warrantycondition" },
-      { text: "Date of Purchase", value: "datepurchase" },
-      { text: "Demand Replacement", value: "demandreplacement" },
-      { text: "Priority", value: "priority" },
+      items: [
+        {
+          date: "Nov 03, 2022 3:56 PM",
+          avatar: "http://10.10.10.38:8080/logo.jpg",
+          title: "BP Service Update",
+          subtitle: `CAN WE REUEST NA KUNIN NLNG PO NAMIN ANG PARTS SA TOTATLINE DAGUPAN SINCE AVIALBLE NAMN PO ANG PARTS DOON MA'AM `,
+        },
+        {
+          date: "Nov 03, 2022 3:56 PM",
+          avatar: "http://10.10.10.38:8080/logo.jpg",
+          title: "Parts",
+          subtitle: `This is approved wty con by Cherry for PCB outdoor, labor c/o customer. already inform customer by Cherry, "clles cx spoke to her husband informed that the wty con for parts approved cx ack" ------ done released to ERP 4001005995 - PCB: 43T6W608 SO #1680275586`,
+        },
       ],
+
+      unitsHeader: [
+        { text: "Product Category", value: "prodcategories" },
+        { text: "Appliance Type", value: "appliancetype" },
+        // { text: "Request Type", value: "branch.name" },
+        { text: "Brand", value: "brand" },
+        { text: "Qty", value: "qty" },
+        { text: "Model", value: "model" },
+        { text: "Serial No.", value: "serialno" },
+        { text: "Unit Condition", value: "unitcondition" },
+        { text: "Warranty Condition", value: "warrantycondition" },
+        { text: "Date of Purchase", value: "datepurchase" },
+        { text: "Demand Replacement", value: "demandreplacement" },
+        { text: "Priority", value: "priority" },
+      ],
+      jobupdateDialog: false,
       unitsData: [],
-      jobstatus: '',
+      tech: "",
+      jobstatus: "",
       jobsInfo: false,
       jobsData: {
         customer: {
-          firstname:	"",
-          lastname:	"",
-          middlename:	"",
-          barangay:	"",
-          contactperson:	"",
-          cpnumber:	"",
-          emailaddress:	"",
-          houseno:	"",
-          mcity:	"",
-          organization:	"",
-          province:	"",
-          specialinstruction:	"",
-          street:	"",
-          telephoneno:	"",
-        }
+          firstname: "",
+          lastname: "",
+          middlename: "",
+          barangay: "",
+          contactperson: "",
+          cpnumber: "",
+          emailaddress: "",
+          houseno: "",
+          mcity: "",
+          organization: "",
+          province: "",
+          specialinstruction: "",
+          street: "",
+          telephoneno: "",
+        },
       },
       search: "",
       data: [],
-      
+
       headers: [
         {
           text: "REF",
@@ -255,10 +372,26 @@ export default {
         { text: "Action", value: "action" },
       ],
       jobstat: [
-        {name: 'Unassigned', value: 0},
-        {name: 'Accepted', value: 1},
-        {name: 'Dispatch to Other ASC', value: 2}
-      ]
+        { name: "Unassigned", value: "Unassigned" },
+        { name: "Accepted", value: "Accepted" },
+        { name: "Dispatch to Other ASC", value: "Dispatch to Other ASC" },
+      ],
+      installer: [
+        { name: "Mike Doe", value: "Mike Doe" },
+        { name: "Linux Doe", value: "Linux Doe" },
+        { name: "James Doe", value: "James Doe" },
+      ],
+      reasonItem: [
+        { name: "Rescheduled", value: "Rescheduled" },
+        { name: "Parts", value: "Parts" },
+        { name: "Under Observation", value: "Under Observation" },
+        { name: "Quotation Approval", value: "Quotation Approval" },
+        { name: "Waiting Permit", value: "Waiting Permit" },
+        { name: "Follow-Up", value: "Follow-Up" },
+        { name: "BP-Service Update", value: "BP-Service Update" },
+        { name: "Unit Replacement", value: "Unit Replacement" },
+
+      ],
     };
   },
 
@@ -275,19 +408,25 @@ export default {
   methods: {
     view(data) {
       var status;
-      if(data.status == 0){
-        status = 'Unassigned'
-      }else if(data.status == 1){
-        status = 'Accepted'
-      }else if(data.status == 2){
-        status = 'Dispatch to Other ASC'
+      if (data.status == 0) {
+        status = "Unassigned";
+      } else if (data.status == 1) {
+        status = "Accepted";
+      } else if (data.status == 2) {
+        status = "Dispatch to Other ASC";
       }
-      this.jobstat = status
-      this.jobstatus = status
+      this.jobstatus = status;
+      //this.jobstat = status;
+
       this.jobsInfo = true;
-      this.jobsData = data
-      this.unitsData = data.units
-     
+      this.jobsData = data;
+      this.unitsData = data.units;
+    },
+    download(id) {
+      alert(id);
+    },
+    jobUpdates() {
+      this.jobupdateDialog = true;
     },
   },
 };
